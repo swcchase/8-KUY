d46a826e2235ad064054651bf77a5780ee324484
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-04-10 10:46:03 -0400

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -171,3 +171,24 @@ function booleanToString(b){
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 
+  The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+  
+  function basicOp(operation, value1, value2)
+{
+switch(operation) {
+  case '+':
+     return value1 + value2;
+    break;
+  case '-':
+     return value1 - value2
+    break;
+  case '*':
+     return value1 * value2
+    break;
+  case '/':
+     return value1 / value2
+}
+}
+
+console.log(basicOp('+', 4, 7))
