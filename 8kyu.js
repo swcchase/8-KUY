4fb893633681862f8f7f06d9d5f09d9880d56475
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-04-15 00:52:16 -0400

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -106,7 +106,7 @@ function maps(x){
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------------------------
 
--- It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
 function removeChar(str) {
   return str.slice(1, -1);
