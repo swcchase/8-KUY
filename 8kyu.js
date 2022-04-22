74e2da7167d87d3e909ba454c640ac318ff749f2
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-04-21 20:54:21 -0400

    Update 8kyu.js
    add cat/dog age calculator

--- 8kyu.js
+++ 8kyu.js
@@ -246,3 +246,33 @@ function isDivisible(n, x, y) {
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------------------
+  
+ //I got them at the same time as kitten/puppy. That was humanYears years ago.
+//Return their respective ages now as [humanYears,catYears,dogYears] 
+  
+  const humanYearsCatYearsDogYears = (humanYears) => {
+  let catYears = 0;
+  let dogYears = 0;
+  
+  for (let i = 1; i <= humanYears; i++) {
+    if (i === 1) {
+      catYears += 15;
+      dogYears += 15;
+    }
+    else if (i === 2) {
+      catYears += 9;
+      dogYears += 9;
+    }
+    else {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+  return [humanYears,catYears,dogYears];
+}
+
+  
+---------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
