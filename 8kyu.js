525e79ae3f1b42f22b155ba75731989638b15809
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-04-22 23:03:48 -0400

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -275,7 +275,7 @@ function isDivisible(n, x, y) {
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------------------
   
-Complete the function which converts a binary number (given as a string) to a decimal number.  
+//Complete the function which converts a binary number (given as a string) to a decimal number.  
 
 function binToDec(bin){
   return parseInt(bin,2);
