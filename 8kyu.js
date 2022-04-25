696002b1daeac9825bb2fbf39f907275d7028537
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-04-25 08:07:43 -0400

    Update 8kyu.js
    Add shorter strings on outside, longer strings on inside

--- 8kyu.js
+++ 8kyu.js
@@ -290,5 +290,8 @@ const invert = array => array.map(num => -num);
   
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------------------
+//Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but
 
-  
+function solution(a, b) {
+  return a.length < b.length ? a + b + a : b + a + b
+} 
