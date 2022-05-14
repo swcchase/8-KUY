6352dcf25e1600f7b2b49feaa52bec6f50231eb0
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-05-13 23:53:15 -0400

    Update 8kyu.js
    add clock function

--- 8kyu.js
+++ 8kyu.js
@@ -464,7 +464,17 @@ function plural(n) {
   
 ----------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------
-  
+ // Clock shows h hours, m minutes and s seconds after midnight.
+
+//Your task is to write a function which returns the time since midnight in milliseconds.
+
+function past(h, m, s){
+  var miliseconds = 0;
+  miliseconds = miliseconds + s * 1000;
+  miliseconds = miliseconds + m * 60000;
+  miliseconds = miliseconds + h * 3600000;
+  return miliseconds;
+}
 ----------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------
   
