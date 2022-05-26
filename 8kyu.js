dd8f29f62e91d87703f759c76e0a8a0aa7116d52
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-05-25 21:50:31 -0400

    Update 8kyu.js
    add sumofdifferences

--- 8kyu.js
+++ 8kyu.js
@@ -536,3 +536,18 @@ function booleanToString(b){
   return b.toString();
 }
   
+----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------
+
+//Your task is to sum the differences between consecutive pairs in the array in descending order. 
+//In descending order: [10, 2, 1]
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+// If the array is empty or the array has only one element the result should be 0
+
+const sumOfDifferences = arr => arr
+  .sort((a, b) => b - a)
+  .map((a, i) => a - arr[i + 1] || 0)
+  .reduce((a, b) => a + b, 0);
+
+----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------
