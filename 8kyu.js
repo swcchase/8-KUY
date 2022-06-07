73cf6d46ca0282e29b75ad56a083707270aaffd6
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-06-06 20:16:34 -0400

    Update 8kyu.js
    add double character

--- 8kyu.js
+++ 8kyu.js
@@ -607,5 +607,8 @@ String.prototype.isUpperCase = function() {
 ----------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------
 
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//"String" -> "SSttrriinngg"
 
 
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
