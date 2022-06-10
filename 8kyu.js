0660f53ecf16c0413eae74f82593f560ae08d849
Cody Chase <92900038+swcchase@users.noreply.github.com>
2022-06-09 23:58:48 -0400

    Update 8kyu.js
    add method to welcome a person

--- 8kyu.js
+++ 8kyu.js
@@ -614,8 +614,17 @@ String.prototype.isUpperCase = function() {
 const doubleChar = (str) => str.split("").map(c => c + c).join("");
 
 -------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 function removeEveryOther(arr){
   return arr.filter(function(elem, index) {
     return index % 2 === 0;
   });
+}
+
+-------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
+//Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+function sayHello( name, city, state ) {
+return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
 }
